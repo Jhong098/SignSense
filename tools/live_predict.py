2c58ce7dd85637fd2701f8e89674fc4161434592
Jack <jack@ericksons.ca>
2021-02-26 17:52:05 -0500

    Live predict now shows prediction label instead of index

--- tools/live_predict.py
+++ tools/live_predict.py
@@ -11,6 +11,8 @@ import keras
 import holistic
 import train
 
+labels = ['A', 'B', 'C', 'Z']
+
 
 def live_predict(model, use_holistic):
     cap = cv2.VideoCapture(0)
@@ -25,6 +27,7 @@ def live_predict(model, use_holistic):
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     mp_drawing = mp.solutions.drawing_utils
 
+    delay = 0
     window = None
     for image, results in holistic.process_capture(cap, use_holistic):
         row = holistic.to_landmark_row(results, use_holistic)
@@ -33,13 +36,21 @@ def live_predict(model, use_holistic):
         # Discard oldest frame and append new frame to data window
         window[:-1] = window[1:]
         window[-1] = np.array(row)
-        
+
         out = model(np.array([window]))
         prediction = np.argmax(out)
-        print("{} {}%".format(prediction, out[0][prediction]))
+
+        try:
+            if delay == 30:
+                print("{} {}%".format(labels[prediction], out[0][prediction]))
+                delay = 0
+            delay += 1
+        except:
+            continue
 
         holistic.draw_landmarks(image, results, use_holistic)
         cv2.imshow("MediaPipe", image)
+
 
 if __name__ == "__main__":
     model_path = argv[1]
