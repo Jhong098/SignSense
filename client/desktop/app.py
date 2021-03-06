49aa04c41d258020b770e6ffebbf193e037746c9
Jason Hong <jhong098@gmail.com>
2021-03-05 22:21:58 -0800

    try to run multiple processes for receiving and sending

--- client/desktop/app.py
+++ client/desktop/app.py
@@ -1,108 +1,149 @@
 
 
 import socket
+import socketserver
 import threading
+from sys import argv
+import cv2
+import mediapipe as mp
+import itertools
+import numpy as np
+import time
+import signal
+import sys
+from multiprocessing import Queue, Process
+from queue import Empty
+import atexit
+from math import ceil
+
+sys.path.insert(1, './tools')
+import holistic
 
 # Define the message to the server
-msgFromClient       = "Hello UDP Server"
-bytesToSend         = str.encode(msgFromClient)
+# msgFromClient       = "Hello UDP Server"
+# bytesToSend         = str.encode(msgFromClient)
 
 # Buffer size for receiving the datagrams from server
 bufferSize          = 1024
 
 # Server IP address and Port number
 serverAddressPort   = ("127.0.0.1", 9999)
+receiveAddressPort = ("127.0.0.1", 9998)
 
 # Connect2Server forms the thread - for each connection made to the server
 
-def Connect2Server():
+class MyUDPRequestHandler(socketserver.DatagramRequestHandler):
+    # Override the handle() method
+
+    def handle(self):
+        # Receive and print the datagram received from client
+        print("Recieved one request from {}".format(self.client_address[0]))
+
+        datagram = self.rfile.readline().strip()
+
+        print("Datagram Received from server is:".format(datagram))
+        print(datagram)
+
+def Connect2Server(message):
     #Create a socket instance - A datagram socket
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
     # Send message to server using created UDP socket
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    UDPClientSocket.sendto(message, serverAddressPort)
 
     # Receive message from the server
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    msg = "Message from Server {}".format(msgFromServer[0])
+    # msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    # msg = "Message from Server {}".format(msgFromServer[0])
 
-    print(msg)
+    # print(msg)
 
 
-print("Client - Main thread started")  
+# print("Client - Main thread started")
 
-ThreadList  = []
-ThreadCount = 20
+# ThreadList  = []
+# ThreadCount = 20
  
 
-# Create as many connections as defined by ThreadCount
+# # Create as many connections as defined by ThreadCount
 
-for index in range(ThreadCount):
-    ThreadInstance = threading.Thread(target=Connect2Server())
-    ThreadList.append(ThreadInstance)
-    ThreadInstance.start()
+# for index in range(ThreadCount):
+#     ThreadInstance = threading.Thread(target=Connect2Server())
+#     ThreadList.append(ThreadInstance)
+#     ThreadInstance.start()
 
-# Main thread to wait till all connection threads are complete
+# # Main thread to wait till all connection threads are complete
 
-for index in range(ThreadCount):
-    ThreadList[index].join()
+# for index in range(ThreadCount):
+#     ThreadList[index].join()
 
-# def video_loop(feature_q, prediction_q, use_holistic):
-#     cap = cv2.VideoCapture(0)
-#     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-#     cap.set(cv2.CAP_PROP_FOURCC, fourcc)
-#     if not cap.isOpened():
-#         print("Error opening Camera")
-#     fps = cap.get(cv2.CAP_PROP_FPS)
-#     print("Webcam FPS = {}".format(fps))
+def video_loop(use_holistic):
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    cap.set(cv2.CAP_PROP_FOURCC, fourcc)
+    if not cap.isOpened():
+        print("Error opening Camera")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print("Webcam FPS = {}".format(fps))
 
-#     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-#     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#     mp_drawing = mp.solutions.drawing_utils
-#     print("Awaiting start signal from predict")
-#     prediction_q.get()
-#     timestamp = None
-#     delay = 0
-#     tag = ''
-#     print("starting image cap")
-#     for image, results in holistic.process_capture(cap, use_holistic):
-#         newtime = time.time()
-#         if timestamp is not None:
-#             diff = newtime - timestamp
-#             # Uncomment to print time between each frame
-#             # print(diff)
-#         timestamp = newtime
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    mp_drawing = mp.solutions.drawing_utils
+    print("Awaiting start signal from predict")
+    # prediction_q.get()
+    timestamp = None
+    delay = 0
+    tag = ''
+    print("starting image cap")
+    for image, results in holistic.process_capture(cap, use_holistic):
+        newtime = time.time()
+        if timestamp is not None:
+            diff = newtime - timestamp
+            # Uncomment to print time between each frame
+            # print(diff)
+        timestamp = newtime
 
-#         row = holistic.to_landmark_row(results, use_holistic)
-#         feature_q.put(np.array(row))
+        row = holistic.to_landmark_row(results, use_holistic)
+        # feature_q.put(np.array(row))
+        send_feature_thread = threading.Thread(target=Connect2Server(np.array(row).tobytes()))
+        send_feature_thread.start()
+        send_feature_thread.join()
 
-#         try:
-#             out = prediction_q.get_nowait()
-#             prediction = np.argmax(out)
-#             if delay >= PRINT_FREQ:
-#                 if out[prediction] > .8:
-#                     print("{} {}%".format(
-#                         LABELS[prediction], out[prediction]*100))
-#                     tag = LABELS[prediction]
-#                 else:
-#                     print("None ({} {}% Below threshold)".format(
-#                         LABELS[prediction], out[prediction]*100))
+        # try:
+        #     out = prediction_q.get_nowait()
+        #     prediction = np.argmax(out)
+        #     if delay >= PRINT_FREQ:
+        #         if out[prediction] > .8:
+        #             print("{} {}%".format(
+        #                 LABELS[prediction], out[prediction]*100))
+        #             tag = LABELS[prediction]
+        #         else:
+        #             print("None ({} {}% Below threshold)".format(
+        #                 LABELS[prediction], out[prediction]*100))
 
-#                 delay = 0
-#                 if feature_q.qsize() > 5:
-#                     print(
-#                         "Warning: Model feature queue overloaded - size = {}".format(feature_q.qsize()))
-#         except Empty:
-#             pass
+        #         delay = 0
+        #         if feature_q.qsize() > 5:
+        #             print(
+        #                 "Warning: Model feature queue overloaded - size = {}".format(feature_q.qsize()))
+        # except Empty:
+        #     pass
 
-#         delay += 1
+        delay += 1
 
-#         holistic.draw_landmarks(image, results, use_holistic, tag)
-#         cv2.imshow("MediaPipe", image)
+        holistic.draw_landmarks(image, results, use_holistic, tag)
+        cv2.imshow("MediaPipe", image)
 
-# if __name__ == "__main__":
-#     # Use MP Hands only
-#     f_q = Queue()
-#     p = Process(target=video_loop, args=(f_q, p_q, use_holistic,))
-#     atexit.register(exit_handler, p)
-#     p.start()
+def start_server():
+    # Create a Server Instance
+    UDPServerObject = socketserver.ThreadingUDPServer(receiveAddressPort, MyUDPRequestHandler)
+
+    # Make the server wait forever serving connections
+    UDPServerObject.serve_forever()
+
+if __name__ == "__main__":
+    # Use MP Hands only
+    p = Process(target=video_loop(False))
+    p.daemon = True
+    atexit.register(exit_handler, p)
+    p.start()
+
+    start_server()
--- server/app.py
+++ server/app.py
@@ -1,40 +1,193 @@
 
 # Sample UDP Server - Multi threaded
 
- 
-
 # Import the necessary python modules
 import socketserver
+import socket
 import threading
+import pathlib
+from sys import path, argv
+path.insert(1, './tools')
+
+from multiprocessing import Queue, Process
+from queue import Empty
+import atexit
+from math import ceil
+
+import tensorflow as tf
+import keras
+import train
  
 # Create a tuple with IP Address and Port Number
 ServerAddress = ("127.0.0.1", 9999)
+receiveAddressPort = ("127.0.0.1", 9998)
 
-# Subclass the DatagramRequestHandler
+# current working directory
+CURRENT_WORKING_DIRECTORY = pathlib.Path().absolute()
+DEFAULT_MODEL = 'holds_model2'
+
+f_q = Queue()
+p_q = Queue()
+
+LABELS = [None, 'A', 'B', 'C', 'Z']
+
+PRINT_FREQ = 30
+PRED_FREQ = 5
+assert PRINT_FREQ % PRED_FREQ == 0
+
+
+class Message():
+    def __init__(self, data, address):
+        self.data = data
+        self.address = address
+
+class Error(Exception):
+    pass
 
 class MyUDPRequestHandler(socketserver.DatagramRequestHandler):
     # Override the handle() method
 
     def handle(self):
         # Receive and print the datagram received from client
-        print("Recieved one request from {}".format(self.client_address[0]))
+        # print("Recieved one request from {}".format(self.client_address[0]))
 
         datagram = self.rfile.readline().strip()
 
-        print("Datagram Recieved from client is:".format(datagram))
-        print(datagram)  
+        # print("Datagram Received from client is:".format(datagram))
+        # print(datagram)
+
+        f_q.put(datagram)
+        # print(f"f_q len: {f_q.qsize()}")
 
         # Print the name of the thread
-
-        print("Thread Name:{}".format(threading.current_thread().name))
-
-        # Send a message to the client
-
-        self.wfile.write("Message from Server! Hello Client".encode())
+        # print("Thread Name:{}".format(threading.current_thread().name))
 
 
-# Create a Server Instance
-UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress, MyUDPRequestHandler)
+        # # Send a message to the client
+        # self.wfile.write("Message from Server! Hello Client".encode())
 
-# Make the server wait forever serving connections
-UDPServerObject.serve_forever()
+
+def predict_loop(model_path):
+    print("Starting prediction init")
+    train.init_gpu()
+    model = keras.models.load_model(model_path)
+
+    delay = 0
+    window = None
+    results = None
+    results_len = ceil(PRINT_FREQ / PRED_FREQ)
+    print()
+    print("====================Starting prediction===============")
+    print()
+    while True:
+        row = f_q.get()
+        print("got smt")
+        if window is None:
+            window = np.zeros((train.TIMESTEPS, len(row)))
+
+        # Discard oldest frame and append new frame to data window
+        window[:-1] = window[1:]
+        window[-1] = row
+
+        if delay >= PRED_FREQ:
+            out = model(np.array([window]))
+            if results is None:
+                results = np.zeros((results_len, len(LABELS)))
+            results[:-1] = results[1:]
+            results[-1] = out
+
+            p_q.put(np.mean(results, axis=0))
+            print("predicted smt")
+            delay = 0
+        delay += 1
+
+
+def prediction_watcher():
+    print()
+    print("====================Started prediction watcher===============")
+    print()
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    delay = 0
+    while True:
+        try:
+            if delay >= PRINT_FREQ:
+                out = p_q.get_nowait()
+                prediction = np.argmax(out)
+                # send confident prediction
+                if out[prediction] > .8:
+                    print("{} {}%".format(
+                        LABELS[prediction], out[prediction]*100))
+                    tag = LABELS[prediction]
+                    UDPClientSocket.sendto(str.encode(tag), receiveAddressPort)
+                else:
+                    print("None ({} {}% Below threshold)".format(
+                        LABELS[prediction], out[prediction]*100))
+
+                delay = 0
+                if f_q.qsize() > 5:
+                    print(
+                        "Warning: Model feature queue overloaded - size = {}".format(f_q.qsize()))
+        except Empty:
+            pass
+
+        delay += 1
+
+# processes = []
+
+def live_predict(model_path, use_holistic):
+    predict_watcher = Process(target=prediction_watcher, args=())
+    atexit.register(exit_handler, predict_watcher)
+    predict_watcher.daemon = True
+    predict_watcher.start()
+
+    udp_server = Process(target=start_server, args=())
+    atexit.register(exit_handler, udp_server)
+    udp_server.daemon = True
+    udp_server.start()
+    # start_server()
+    
+    # processes.append(predict_watcher)
+
+    predict_loop(model_path)
+    # predict_processor = Process(target=predict_loop, args=(model_path,))
+    # atexit.register(exit_handler, predict_processor)
+    # predict_processor.start()
+    # processes.append(predict_processor)
+
+
+def exit_handler(p):
+    try:
+        p.kill()
+    except:
+        print("Couldn't kill")
+
+
+def start_server():
+    print()
+    print("====================starting server===============")
+    print()
+    # Create a Server Instance
+    UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress, MyUDPRequestHandler)
+
+    # Make the server wait forever serving connections
+    UDPServerObject.serve_forever()
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        model_path = CURRENT_WORKING_DIRECTORY/'models'/DEFAULT_MODEL
+        if not model_path.exists():
+            raise Error("NO MODEL CAN BE USED!")
+    else:
+        model_path = argv[1]    
+    
+    print(f"using model {model_path}")
+
+    # udp_server = Process(target=start_server, args=())
+    # atexit.register(exit_handler, udp_server)
+    # udp_server.daemon = True
+    # udp_server.start()
+
+    # Use MP Hands only
+    live_predict(model_path, False)
+
+    
