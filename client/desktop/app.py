be815d248c04b2701eab998bd4be8869d38adce3
Jason Hong <Jhong098@gmail.com>
2021-03-06 23:55:06 -0800

    Desktop Client and Server (#2)
    * try to run multiple processes for receiving and sending
    
    * working single client-server

--- client/desktop/app.py
+++ client/desktop/app.py
@@ -1,108 +1,119 @@
-
-
 import socket
+import socketserver
 import threading
+from sys import argv
+import cv2
+import mediapipe as mp
+import itertools
+import numpy as np
+import time
+import signal
+import sys
+from multiprocessing import Queue, Process
+from queue import Empty
+import atexit
+from math import ceil
+import asyncio
 
-# Define the message to the server
-msgFromClient       = "Hello UDP Server"
-bytesToSend         = str.encode(msgFromClient)
+sys.path.insert(1, './tools')
+import holistic, common
 
-# Buffer size for receiving the datagrams from server
-bufferSize          = 1024
+PRINT_FREQ = 30
 
 # Server IP address and Port number
 serverAddressPort   = ("127.0.0.1", 9999)
+receiveAddressPort = ("127.0.0.1", 9998)
 
-# Connect2Server forms the thread - for each connection made to the server
+APP_NAME = "SignSense"
 
-def Connect2Server():
+class PredictionReceiver(common.UDPRequestHandler):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.__dict__.update(kwargs)
+
+    def datagram_received(self, data, addr):
+        # Receive and print the datagram received from client
+        # print(f"received datagram from {addr}")
+        try:
+            datagram = data.decode()
+            print(f"Datagram Received from client is: {datagram}")
+            self.p_q.put(datagram)
+        except:
+            print("exception while receiving datagram")
+            pass
+
+# send UDP datagram
+def Connect2Server(message):
     #Create a socket instance - A datagram socket
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
     # Send message to server using created UDP socket
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    UDPClientSocket.sendto(message, serverAddressPort)
 
     # Receive message from the server
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    msg = "Message from Server {}".format(msgFromServer[0])
-
-    print(msg)
+    # msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    # msg = "Message from Server {}".format(msgFromServer[0])
 
 
-print("Client - Main thread started")  
+def video_loop(p_q, use_holistic=False):
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    cap.set(cv2.CAP_PROP_FOURCC, fourcc)
+    if not cap.isOpened():
+        print("Error opening Camera")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print("Webcam FPS = {}".format(fps))
 
-ThreadList  = []
-ThreadCount = 20
- 
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    mp_drawing = mp.solutions.drawing_utils
 
-# Create as many connections as defined by ThreadCount
+    timestamp = None
+    predicted = None
+    delay = 0
+    tag = ''
+    print("starting image cap")
 
-for index in range(ThreadCount):
-    ThreadInstance = threading.Thread(target=Connect2Server())
-    ThreadList.append(ThreadInstance)
-    ThreadInstance.start()
+    for image, results in holistic.process_capture(cap, use_holistic):
+        newtime = time.time()
+        if timestamp is not None:
+            diff = newtime - timestamp
+            # Uncomment to print time between each frame
+            # print(diff)
+        timestamp = newtime
 
-# Main thread to wait till all connection threads are complete
+        row = holistic.to_landmark_row(results, use_holistic)
+        # feature_q.put(np.array(row))
+        send_feature_thread = threading.Thread(target=Connect2Server(np.array(row).tobytes()))
+        send_feature_thread.start()
+        send_feature_thread.join()
 
-for index in range(ThreadCount):
-    ThreadList[index].join()
+        try:
+            out = p_q.get_nowait()
+            if delay >= PRINT_FREQ:
+                if out:
+                    predicted = out
+                delay = 0
+        except Empty:
+            pass
 
-# def video_loop(feature_q, prediction_q, use_holistic):
-#     cap = cv2.VideoCapture(0)
-#     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-#     cap.set(cv2.CAP_PROP_FOURCC, fourcc)
-#     if not cap.isOpened():
-#         print("Error opening Camera")
-#     fps = cap.get(cv2.CAP_PROP_FPS)
-#     print("Webcam FPS = {}".format(fps))
+        delay += 1
 
-#     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-#     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#     mp_drawing = mp.solutions.drawing_utils
-#     print("Awaiting start signal from predict")
-#     prediction_q.get()
-#     timestamp = None
-#     delay = 0
-#     tag = ''
-#     print("starting image cap")
-#     for image, results in holistic.process_capture(cap, use_holistic):
-#         newtime = time.time()
-#         if timestamp is not None:
-#             diff = newtime - timestamp
-#             # Uncomment to print time between each frame
-#             # print(diff)
-#         timestamp = newtime
+        holistic.draw_landmarks(image, results, use_holistic, predicted)
+        cv2.imshow(APP_NAME, image)
 
-#         row = holistic.to_landmark_row(results, use_holistic)
-#         feature_q.put(np.array(row))
+if __name__ == "__main__":
+    # queue containing the returned predictions from the server
+    p_q = Queue()
 
-#         try:
-#             out = prediction_q.get_nowait()
-#             prediction = np.argmax(out)
-#             if delay >= PRINT_FREQ:
-#                 if out[prediction] > .8:
-#                     print("{} {}%".format(
-#                         LABELS[prediction], out[prediction]*100))
-#                     tag = LABELS[prediction]
-#                 else:
-#                     print("None ({} {}% Below threshold)".format(
-#                         LABELS[prediction], out[prediction]*100))
+    # start separate process for the webcam video GUI
+    p = Process(target=video_loop, args=(p_q,))
+    p.daemon = True
+    atexit.register(common.exit_handler, p)
+    p.start()
 
-#                 delay = 0
-#                 if feature_q.qsize() > 5:
-#                     print(
-#                         "Warning: Model feature queue overloaded - size = {}".format(feature_q.qsize()))
-#         except Empty:
-#             pass
-
-#         delay += 1
-
-#         holistic.draw_landmarks(image, results, use_holistic, tag)
-#         cv2.imshow("MediaPipe", image)
-
-# if __name__ == "__main__":
-#     # Use MP Hands only
-#     f_q = Queue()
-#     p = Process(target=video_loop, args=(f_q, p_q, use_holistic,))
-#     atexit.register(exit_handler, p)
-#     p.start()
+    # run UDP server to receive predictions
+    asyncio.run(common.start_server(
+        PredictionReceiver(p_q=p_q),
+        receiveAddressPort
+    ))
--- server/app.py
+++ server/app.py
@@ -1,40 +1,180 @@
 
-# Sample UDP Server - Multi threaded
-
- 
-
-# Import the necessary python modules
 import socketserver
+import socket
 import threading
+import pathlib
+from sys import path, argv
+
+path.insert(1, './tools')
+import common
+
+from multiprocessing import Queue, Process
+from queue import Empty, Full
+import atexit
+from math import ceil
+import numpy as np
+import asyncio
+
+import tensorflow as tf
+import keras
+import train
  
 # Create a tuple with IP Address and Port Number
 ServerAddress = ("127.0.0.1", 9999)
+receiveAddressPort = ("127.0.0.1", 9998)
 
-# Subclass the DatagramRequestHandler
+# current working directory
+CURRENT_WORKING_DIRECTORY = pathlib.Path().absolute()
 
-class MyUDPRequestHandler(socketserver.DatagramRequestHandler):
-    # Override the handle() method
+DEFAULT_MODEL = 'holds_model2'
 
-    def handle(self):
+# TODO: make this dynamic according to the data/videos?
+# prediction condigurations
+LABELS = [None, 'A', 'B', 'C', 'Z']
+
+PRINT_FREQ = 30
+PRED_FREQ = 5
+MAX_QUEUE_LEN = 50
+
+class Message():
+    def __init__(self, data, address):
+        self.data = data
+        self.address = address
+
+class Error(Exception):
+    pass
+
+# TODO: store landmarks based on the client to handle multiple clients
+class LandmarkReceiver(common.UDPRequestHandler):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.__dict__.update(kwargs)
+
+    def datagram_received(self, data, addr):
         # Receive and print the datagram received from client
-        print("Recieved one request from {}".format(self.client_address[0]))
+        # print(f"received datagram from {addr}")
+        try:
+            datagram = np.frombuffer(data)
 
-        datagram = self.rfile.readline().strip()
+            # print("Datagram Received from client is:".format(datagram))
+            # print(datagram)
 
-        print("Datagram Recieved from client is:".format(datagram))
-        print(datagram)  
-
-        # Print the name of the thread
-
-        print("Thread Name:{}".format(threading.current_thread().name))
-
-        # Send a message to the client
-
-        self.wfile.write("Message from Server! Hello Client".encode())
+            self.f_q.put_nowait(datagram)
+        except Full:
+            # print("exception while receiving datagram")
+            pass
 
 
-# Create a Server Instance
-UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress, MyUDPRequestHandler)
+def predict_loop(model_path, f_q, p_q):
+    train.init_gpu()
+    model = keras.models.load_model(model_path)
 
-# Make the server wait forever serving connections
-UDPServerObject.serve_forever()
+    delay = 0
+    window = None
+    results = None
+    results_len = ceil(PRINT_FREQ / PRED_FREQ)
+
+    p_q.put("start")
+
+    print()
+    print("====================Starting prediction===============")
+    print()
+
+    while True:
+        row = f_q.get()
+        
+        if window is None:
+            window = np.zeros((train.TIMESTEPS, len(row)))
+
+        # Discard oldest frame and append new frame to data window
+        window[:-1] = window[1:]
+        window[-1] = row
+
+        if delay >= PRED_FREQ:
+            out = model(np.array([window]))
+            if results is None:
+                results = np.zeros((results_len, len(LABELS)))
+            results[:-1] = results[1:]
+            results[-1] = out
+
+            p_q.put(np.mean(results, axis=0))
+            delay = 0
+    
+        delay += 1
+
+
+def prediction_watcher(f_q, p_q):
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    delay = 0
+
+    p_q.get()
+
+    print()
+    print("====================Started prediction watcher===============")
+    print()
+
+    while True:
+        try:
+            if delay >= PRINT_FREQ:
+                out = p_q.get_nowait()
+                prediction = np.argmax(out)
+                # send confident prediction
+                if out[prediction] > .8:
+                    print("{} {}%".format(
+                        LABELS[prediction], out[prediction]*100))
+                    tag = LABELS[prediction]
+
+                    # send back prediction if it is a valid class
+                    if tag is not None:
+                        UDPClientSocket.sendto(tag.encode(), receiveAddressPort)
+                else:
+                    print("None ({} {}% Below threshold)".format(
+                        LABELS[prediction], out[prediction]*100))
+
+                delay = 0
+                if f_q.qsize() > 5:
+                    print(
+                        "Warning: Model feature queue overloaded - size = {}".format(f_q.qsize()))
+        except Empty:
+            pass
+
+        delay += 1
+
+def live_predict(model_path, use_holistic):
+    # queue containing the landmark features from the client
+    f_q = Queue(MAX_QUEUE_LEN)
+
+    # queue containing the predictions
+    p_q = Queue(MAX_QUEUE_LEN)
+
+    # launch watcher process for checking and sending predictions back
+    predict_watcher = Process(target=prediction_watcher, args=(f_q, p_q,))
+    atexit.register(common.exit_handler, predict_watcher)
+    predict_watcher.daemon = True
+    predict_watcher.start()
+
+    # launch process for predictions
+    predict = Process(target=predict_loop, args=(model_path, f_q, p_q,))
+    atexit.register(common.exit_handler, predict)
+    predict.daemon = True
+    predict.start()
+    
+    # launch UDP server to receive landmark features
+    asyncio.run(common.start_server(
+        LandmarkReceiver(f_q=f_q, p_q=p_q),
+        ServerAddress
+    ))
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        model_path = CURRENT_WORKING_DIRECTORY/'models'/DEFAULT_MODEL
+        if not model_path.exists():
+            raise Error("NO MODEL CAN BE USED!")
+    else:
+        model_path = argv[1]    
+    
+    print(f"using model {model_path}")
+
+    live_predict(model_path, False)
+
+    
